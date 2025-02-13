64ae2fbaa297a38cf71bafde6ae904f353e6eee5
Shikharagrawal-2408 <shikharsatna05@gmail.com>
2025-02-13 11:31:42 +0530

    ques 12

---
+++ ques12/code.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class code {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("initial value of a "+a); 
+        System.out.println("initial value of b "+b); 
+        a = a + b; 
+        b = a - b; 
+        a = a - b; 
+         System.out.println("changed value of a "+a); 
+        System.out.println("changed value of b "+b);
+    }
+}
