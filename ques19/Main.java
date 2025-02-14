32eef0a2f789cc44900170587ad9d34146d1cf87
Shikharagrawal-2408 <shikharsatna05@gmail.com>
2025-02-14 18:14:10 +0530

    ques19

--- ques19/Main.java
+++ ques19/Main.java
@@ -3,6 +3,7 @@ package cipherschooljava.ques19;
 import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
